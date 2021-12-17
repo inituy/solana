6268ed9b25d433d03ca662432f7896d147a9fc83
SF-Prog <fitipaldosantiago@gmail.com>
2021-12-17 03:12:52 -0300

    Removed anchor for minting an NFT[WIP]

--- test6/app/purchase_nft.js
+++ test6/app/purchase_nft.js
@@ -14,14 +14,16 @@ const SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID = new solana.PublicKey(
 const TOKEN_METADATA_PROGRAM_ID = new solana.PublicKey(
   'metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s',
 );
-
-var mint = solana.Keypair.generate()
+// TODO: shouldn't this walled be in some way associated with the candy machine?
 
 module.exports = function (payload) {
+  console.log('nftCandyMachine', payload.nftCandyMachine);
+  var fromWallet = payload.creator;
+  var toWallet = payload.receiver;
   return Promise.resolve(payload)
     .then(function () {
       // Finds ATA between buyer and NFT
-      var owner = payload.owner;
+      var owner = toWallet;
       return solana.PublicKey.findProgramAddress(
         [owner.publicKey.toBuffer(), splToken.TOKEN_PROGRAM_ID.toBuffer(), mint.publicKey.toBuffer()],
         SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID
@@ -40,42 +42,100 @@ module.exports = function (payload) {
       )
     })
     .then(function (params) {
-      payload.candyMachineAddress = params[0]
-      return payload
+      payload.candyMachineAddress = params[0];
+      return payload;
     })
-    .then(async function (data) {
-      const wallet = new anchor.Wallet(payload.candyMachineAddress)
-      const provider = new anchor.Provider(payload.connection, wallet, {
-        preflightCommitment: 'recent',
-      })
-      const idl = await anchor.Program.fetchIdl(CANDY_MACHINE_PROGRAM_ID, provider)
-      payload.candyMachineIdl = idl
-      payload.candyMachineProvider = provider
-      return payload
-    })
-    .then(function () {
-      const program = new anchor.Program(
-        payload.candyMachineIdl,
-        CANDY_MACHINE_PROGRAM_ID,
-        payload.candyMachineProvider
-      )
-      payload.loadCandyProgram = program
-      return payload
+    // .then(async function (data) {
+    //   const wallet = new anchor.Wallet(payload.candyMachineAddress)
+    //   const provider = new anchor.Provider(payload.connection, wallet, {
+    //     preflightCommitment: 'recent',
+    //   });
+    //   const idl = await solana.SystemProgram.;
+    //   payload.candyMachineIdl = idl;
+    //   payload.candyMachineProvider = provider;
+    //   return payload;
+    // })
+    // .then(function () {
+    //   const program = new solana.SystemProgram(
+    //     payload.candyMachineIdl,
+    //     CANDY_MACHINE_PROGRAM_ID,
+    //     payload.candyMachineProvider
+    //   );
+    //   console.log('program', program);
+    //   payload.loadCandyProgram = program;
+    //   return payload;
+    // })
+    // .then(async function () {
+    //   payload.candyMachine = await payload.loadCandyProgram.account.candyMachine.fetch(
+    //     payload.candyMachineAddress
+    //   );
+    //   return payload;
+    // })
+    .then(async function () {
+      // Optional ??
+      var fromAirdropSignature = await payload.connection.requestAirdrop(
+        fromWallet.publicKey,
+        solana.LAMPORTS_PER_SOL,
+      );
+      //wait for airdrop confirmation
+      await payload.connection.confirmTransaction(fromAirdropSignature);
+      return payload;
     })
     .then(async function () {
-      payload.candyMachine = await payload.loadCandyProgram.account.candyMachine.fetch(
-        payload.candyMachineAddress
-      )
-      return payload
+      payload.mint = await splToken.Token.createMint(
+        payload.connection,
+        // Currently set for the buyer(owner) to pay the fee. TODO: Make the CM pay the fee
+        toWallet,
+        fromWallet.publicKey,
+        null,
+        9,
+        splToken.TOKEN_PROGRAM_ID,
+      );
+      return payload;
     })
+    .then(async function () {
+      // account on the CM side
+      payload.fromTokenAccount = await payload.mint.getOrCreateAssociatedAccountInfo(
+        fromWallet.publicKey,
+      );
+      // receiver's account
+      payload.toWalletAccount = await payload.mint.getOrCreateAssociatedAccountInfo(
+        toWallet.publicKey,
+      );
+      return payload;
+    })
+    .then(async function() {
+      //minting 1 new token to the "fromTokenAccount" account we just returned/created
+      await payload.mint.mintTo(
+        payload.fromTokenAccount.address, //who it goes to
+        fromWallet.publicKey, // minting authority
+        [], // multisig
+        1000000000, // how many
+      );
+      return payload;
+    })
+    .then(async function() {
+      // will revoke minting privileges and ensure that we can not create additional tokens of this type
+      await payload.mint.setAuthority(
+        payload.mint.publicKey,
+        null,
+        "MintTokens",
+        fromWallet.publicKey,
+        []
+      );
+      return payload;
+    })
+
+
+
     .then(async function (params) {
       const instructions = [
         solana.SystemProgram.createAccount({
-          fromPubkey: payload.owner.publicKey,
-          newAccountPubkey: mint.publicKey,
+          fromPubkey: toWallet.publicKey,
+          newAccountPubkey: fromWallet.publicKey,
           space: splToken.MintLayout.span,
           lamports:
-            await payload.loadCandyProgram.provider.connection.getMinimumBalanceForRentExemption(
+            await payload.connection.getMinimumBalanceForRentExemption(
               splToken.MintLayout.span
             ),
           programId: splToken.TOKEN_PROGRAM_ID
@@ -83,29 +143,29 @@ module.exports = function (payload) {
 
         splToken.Token.createInitMintInstruction(
           splToken.TOKEN_PROGRAM_ID,
-          mint.publicKey,
+          fromWallet.publicKey,
           0,
-          payload.owner.publicKey,
-          payload.owner.publicKey,
+          toWallet.publicKey,
+          toWallet.publicKey,
         ),
 
         createAssociatedTokenAccountInstruction(
           payload.userTokenAccountAddress,
-          payload.owner.publicKey,
-          payload.owner.publicKey,
-          mint.publicKey
+          toWallet.publicKey,
+          toWallet.publicKey,
+          fromWallet.publicKey
         ),
 
         splToken.Token.createInitMintInstruction(
           splToken.TOKEN_PROGRAM_ID,
-          mint.publicKey,
+          fromWallet.publicKey,
           1,
           payload.userTokenAccountAddress,
-          payload.owner.publicKey,
+          toWallet.publicKey,
         ),
       ];
       payload.instructions = instructions;
-      return payload
+      return payload;
     })
     .then(function (params) {
       payload.tokenAccount;
@@ -115,7 +175,7 @@ module.exports = function (payload) {
 
         payload.tokenAccount = solana.PublicKey.findProgramAddress(
          [
-           payload.owner.publicKey.toBuffer(),
+           toWallet.publicKey.toBuffer(),
            splToken.TOKEN_PROGRAM_ID.toBuffer(),
            payload.candyMachine.tokenMint.toBuffer()
          ],
@@ -128,7 +188,7 @@ module.exports = function (payload) {
           isSigner: false,
         });
         remainingAccounts.push({
-          pubkey: payload.owner.publicKey,
+          pubkey: toWallet.publicKey,
           isWritable: false,
           isSigner: true,
         });
@@ -138,7 +198,7 @@ module.exports = function (payload) {
             splToken.TOKEN_PROGRAM_ID,
             payload.tokenAccount,
             transferAuthority.publicKey,
-            payload.owner.publicKey,
+            toWallet.publicKey,
             [],
             payload.candyMachine.data.price.toNumber(),
           ),
@@ -146,14 +206,16 @@ module.exports = function (payload) {
         payload.remainingAccounts = remainingAccounts
       }
       payload.remainingAccounts = [];
-      return payload
+      return payload;
     })
+
+
     .then(async function () {
       const metadataAddress = await solana.PublicKey.findProgramAddress(
         [
           Buffer.from('metadata'),
           TOKEN_METADATA_PROGRAM_ID.toBuffer(),
-          mint.publicKey.toBuffer(),
+          fromWallet.publicKey.toBuffer(),
           Buffer.from('edition'),
         ],
         TOKEN_METADATA_PROGRAM_ID,
@@ -162,54 +224,44 @@ module.exports = function (payload) {
         [
           Buffer.from('metadata'),
           TOKEN_METADATA_PROGRAM_ID.toBuffer(),
-          mint.publicKey.toBuffer(),
+          fromWallet.publicKey.toBuffer(),
           Buffer.from('edition'),
         ],
         TOKEN_METADATA_PROGRAM_ID,
       )
       var remainingAccounts = payload.remainingAccounts
       payload.instructions.push(
-        payload.loadCandyProgram.instruction.mintNft({
-          accounts: {
-            config: new solana.PublicKey(payload.nftCandyMachine.program.config),
-            candyMachine: payload.candyMachineAddress,
-            payer: payload.owner.publicKey,
-            wallet: payload.candyMachine.wallet,
-            mint: mint.publicKey,
-            metadata: metadataAddress[0],
-            masterEdition: masterEdition[0],
-            mintAuthority: payload.owner.publicKey,
-            updateAuthority: payload.owner.publicKey,
-            tokenMetadataProgram: TOKEN_METADATA_PROGRAM_ID,
-            tokenProgram: splToken.TOKEN_PROGRAM_ID,
-            systemProgram: solana.SystemProgram.programId,
-            rent: solana.SYSVAR_RENT_PUBKEY,
-            clock: solana.SYSVAR_CLOCK_PUBKEY,
-          },
-          remainingAccounts,
-        })
+        splToken.Token.createTransferInstruction(
+          splToken.TOKEN_PROGRAM_ID,
+          fromTokenAccount.address,
+          toTokenAccount.address,
+          fromWallet.publicKey,
+          [],
+          1,
+        ),
       );
       return payload
     })
-    .then(function () {
-      if (payload.tokenAccount) {
-        payload.instructions.push(
-          Token.createRevokeInstruction(
-            splToken.TOKEN_PROGRAM_ID,
-            payload.tokenAccount,
-            payload.owner.publicKey,
-            [],
-          ),
-        );
-      }
-      return payload
-    })
+    // .then(function () {
+    //   if (payload.tokenAccount) {
+    //     payload.instructions.push(
+    //       Token.createRevokeInstruction(
+    //         splToken.TOKEN_PROGRAM_ID,
+    //         payload.tokenAccount,
+    //         toWallet.publicKey,
+    //         [],
+    //       ),
+    //     );
+    //   }
+    //   return payload
+    // })
     .then(function name(params) {
+      
       return sendTransactionWithRetryWithKeypair(
         payload.connection,
-        payload.owner,
+        toWallet,
         payload.instructions,
-        [mint, payload.owner]
+        [mint, toWallet]
       )
     })
 };
